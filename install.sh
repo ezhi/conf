1319bd43be48681a941db241de79b4e9d231d582
Igor Zaytsev <igor.zaytsev@gmail.com>
2011-12-28 20:53:48 +0300

    Use https url

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-cd && rm -rf conf && git clone git://github.com/ezhi/conf.git conf && \
+cd && rm -rf conf && git clone https://ezhi@github.com/ezhi/conf.git conf && \
     ln -sf `find conf -maxdepth 1 -mindepth 1 -name ".*" | grep -vw .git` .
