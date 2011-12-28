69936d532d8eeca27611753d1721d53a3b445fd4
Igor Zaytsev <igor.zaytsev@gmail.com>
2011-12-29 01:40:02 +0400

    pull if ~/conf already exists

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-cd && rm -rf conf && git clone https://ezhi@github.com/ezhi/conf.git conf && \
-    ln -sf `find conf -maxdepth 1 -mindepth 1 -name ".*" | grep -vw .git` .
+( (cd ~/conf && git pull --ff-only) \
+    || (cd ~ && git clone https://ezhi@github.com/ezhi/conf.git conf) ) \
+&& cd ~ && ln -sf `find conf -maxdepth 1 -mindepth 1 -name ".*" | grep -vw .git` .
